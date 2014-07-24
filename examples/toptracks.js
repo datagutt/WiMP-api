3c48452ed2fafb309db034f5fe07d204273bcf46
Thomas Lekanger <datagutt@lekanger.no>
2014-07-24 16:43:42 +0200

    Fully implement search

--- examples/toptracks.js
+++ examples/toptracks.js
@@ -5,7 +5,7 @@ var username = process.env.UN;
 var password = process.env.PW;
 var artistId = 4948323;
 WiMP.login(username, password, function(err, wimp){
-	wimp.search('artists', 'Hvitmalt Gjerde', function(err, artists){
+	wimp.search('artist', 'Hvitmalt Gjerde', function(err, artists){
 		wimp.getTopTracks(artists[0].id, function(err, tracks){
 			var track = tracks[0];
 			console.log('Playing: %s - %s', track.artist.name, track.title);
